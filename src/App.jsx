767f91d18887719c8bbcddb0a75b75bdb9bc9984
Jahidul <jahidul.cf@gmail.com>
2026-03-30 11:52:43 +0600

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className='text-3xl'>hello from react tailwind</h1>
+     <button className='btn'>
+      click
+     </button>
+    </>
+  )
+}
+
+export default App
